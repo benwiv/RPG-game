4c0c3e3c1f4385807639a4d5d5b9c2ee39666ee4
Ben Waller <benwalleriv@gmail.com>
2019-03-03 22:25:13 -0800

    style 95%, functionaly 90% - need to add reset function and clean up a few bugs

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,10 @@
 //  GLOBAL VARIABLES
 //  toons object contains the different character values, including name, health, attack damage and their image relative url
 const toons = {
-  samus: {name: 'samus', health: 180, attack: 25, img:'./assets/images/samus-16bit.png', imgPlayer: './assets/images/samus-16bit-player.png', imgEnemy:'./assets/images/samus-16bit-enemy.png'},
-  mario: {name: 'mario', health: 120, attack: 13, img:'./assets/images/mario-16bit.png', imgPlayer: './assets/images/mario-16bit-player.png', imgEnemy:'./assets/images/mario-16bit-enemy.png'},
-  spidey: {name: 'spidey', health: 100, attack: 10, img:'./assets/images/spidey-16bit.png', imgPlayer: './assets/images/spidey-16bit-player.png', imgEnemy:'./assets/images/spidey-16bit-enemy.png'},
-  sonic: {name: 'sonic', health: 150, attack: 18, img:'./assets/images/sonic-16bit.png', imgPlayer: './assets/images/sonic-16bit-player.png', imgEnemy:'./assets/images/spidey-16bit-enemy.png'},
+  samus: {name: 'samus', health: 180, attack: 20, img:'./assets/images/samus-16bit.png', imgPlayer: './assets/images/samus-16bit-player.png', imgEnemy:'./assets/images/samus-16bit-enemy.png'},
+  mario: {name: 'mario', health: 120, attack: 8, img:'./assets/images/mario-16bit.png', imgPlayer: './assets/images/mario-16bit-player.png', imgEnemy:'./assets/images/mario-16bit-enemy.png'},
+  spidey: {name: 'spidey', health: 100, attack: 14, img:'./assets/images/spidey-16bit.png', imgPlayer: './assets/images/spidey-16bit-player.png', imgEnemy:'./assets/images/spidey-16bit-enemy.png'},
+  sonic: {name: 'sonic', health: 150, attack: 12, img:'./assets/images/sonic-16bit.png', imgPlayer: './assets/images/sonic-16bit-player.png', imgEnemy:'./assets/images/sonic-16bit-enemy.png'},
 }
 
 const toonArr = Object.keys(toons);
@@ -28,14 +28,18 @@ function renderToons() {
 
 renderToons();
 
-//  choose character function
-//  function chooseToon() {
-//  for (i=0; i<enemyArr.length;i++){
-
+//  CHOOSE CHARACTER
 $(document).ready(function() {
   
   const enemyArr = Object.keys(toons);
   let round=0;
+  let heroName = '';
+  let heroHP = 0;
+  let heroATK = 0;
+  let defenderName = '';
+  let defenderHP = 0;
+  let defenderATK = 0;
+
   $('.toon-icon').click(function(){
   if(round===0){
     let playerToon = $(this).find(">:first-child").text();
@@ -44,6 +48,9 @@ $(document).ready(function() {
     enemyArr.splice(enemyArr.indexOf(playerToon),1);
     $('#toon-select').css('width',"750px");
     $(this).remove();
+    heroName = toons[playerToon].name;
+    heroHP = toons[playerToon].health;
+    heroATK = toons[playerToon].attack;
   }
   else if(round===1) {
     let enemyToon = $(this).find(">:first-child").text();
@@ -52,22 +59,62 @@ $(document).ready(function() {
     enemyArr.splice(enemyArr.indexOf(enemyToon),1);
     $('#toon-select').css('width',"500px");
     $(this).remove();
+    defenderName = toons[enemyToon].name;
+    defenderHP = toons[enemyToon].health;
+    defenderATK = toons[enemyToon].attack;
   }
   else if (round===2){
-    alert('final round');
+    let enemyToon = $(this).find(">:first-child").text();
+    let eToonImage = $('<img id="p-toon">').attr('src', toons[enemyToon].imgEnemy);
+    $('#defender-toon').append(eToonImage);
+    enemyArr.splice(enemyArr.indexOf(enemyToon),1);
+    $('#toon-select').css('width',"250px");
+    $(this).remove();
+    defenderName = toons[enemyToon].name;
+    defenderHP = toons[enemyToon].health;
+    defenderATK = toons[enemyToon].attack;
   }
   else{
-    alert('donezo');
+    let enemyToon = $(this).find(">:first-child").text();
+    let eToonImage = $('<img id="p-toon">').attr('src', toons[enemyToon].imgEnemy);
+    $('#defender-toon').append(eToonImage);
+    enemyArr.splice(enemyArr.indexOf(enemyToon),1);
+    $('#toon-select').css('width',"250px");
+    $(this).remove();
+    defenderName = toons[enemyToon].name;
+    defenderHP = toons[enemyToon].health;
+    defenderATK = toons[enemyToon].attack;
   }
   round++;
+  });
+
+
+  $('#attack-btn').click(function(){
+    let defenderImage = $('<img id="p-toon">').attr('src', toons[defenderName].imgEnemy);
+    let heroImage = $('<img id="p-toon">').attr('src', toons[heroName].imgPlayer);
+    let graveWidth = 250*(round-1)
+
+    if(enemyArr.length<=2) {
+      heroHP-=defenderATK;
+      defenderHP-=heroATK;
+      heroATK+=8
+  
+      if (defenderHP<0){
+        console.log('defender dead');
+        $('#enemy-toons').css('width',String(graveWidth))
+        $('#enemy-toons').append(defenderImage);
+        $('#defender-toon #p-toon').attr('src','');
+      }
+      else if (heroHP<=0){
+        console.log('hero dead');
+        $('#enemy-toons').append(heroImage);
+      }
+      else {
+        $('#log-txt').html(heroName + ' hits ' + defenderName + ' for ' + heroATK + ' dmg! ' + defenderName + ' hits back for ' + 
+        defenderATK + ' dmg. ' + heroName + ' is down to ' + heroHP + ' & ' + defenderName + ' is at ' + defenderHP + '...');
+      }
+    }
   });
 });
 
 
-// $(".toon-icon").on("click", function() {
-//   $(".toon-icon").detach().appendTo("#player-toon");
-// });
-// something.onmouseover = function() {
-//   this.style.backgroundColor = 'red';
-// }
-// 'url(./assets/images/sonic-16bit-player.png)'
